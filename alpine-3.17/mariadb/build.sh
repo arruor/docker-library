c3287dbc83824ce0fe54048d8e9893196480d1cf
Dimitar Nikov <dn@20i.com>
2023-05-11 04:12:18 +0100

    Do not use cache when building MariaDB images

--- alpine-3.17/mariadb/build.sh
+++ alpine-3.17/mariadb/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker build -t arruor/mariadb:latest -t arruor/mariadb:10.6 -t arruor/mariadb:10.6.12 .
+docker build --no-cache -t arruor/mariadb:latest -t arruor/mariadb:10.6 -t arruor/mariadb:10.6.12 .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
