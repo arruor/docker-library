88030e6023d5db8f0c9a7dcc40267a7dc9619055
Dimitar Nikov <dn@20i.com>
2023-12-06 12:10:22 +0000

    Fix additional user creation when starting MariaDB via docker-compose.yml

--- alpine/mariadb/startup.sh
+++ alpine/mariadb/startup.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 # execute any pre-init scripts
 for i in /usr/local/bin/pre-init.d/*sh
 do
 	if [ -e "${i}" ]; then
-		echo "[i] pre-init.d - processing $i"
+		echo "[i] pre-init.d - processing ${i}"
+		# shellcheck disable=SC1090
 		. "${i}"
 	fi
 done
 
 if [ -d "/run/mysqld" ]; then
-	echo "[i] mysqld already present, skipping creation"
+	echo "[i] /run/mysqld already present, skipping creation"
 	chown -R mysql:mysql /run/mysqld
 else
-	echo "[i] mysqld not found, creating...."
+	echo "[i] /run/mysqld not found, creating..."
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
 fi
@@ -28,72 +29,88 @@ else
 
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 
-	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-		MYSQL_ROOT_PASSWORD=`pwgen 16 1`
-		echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
+	if [ "${MARIADB_ROOT_PASSWORD}" = "" ]; then
+		MARIADB_ROOT_PASSWORD=$(pwgen 16 1)
+		echo "[i] MySQL root Password: ${MARIADB_ROOT_PASSWORD}"
 	fi
 
-	MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-	MYSQL_USER=${MYSQL_USER:-""}
-	MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+	MARIADB_DATABASE=${MARIADB_DATABASE:-""}
+	MARIADB_USER=${MARIADB_USER:-""}
+	MARIADB_PASSWORD=${MARIADB_PASSWORD:-""}
 
-	tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
+	tfile=$(mktemp)
+	if [ ! -f "${tfile}" ]; then
 	    return 1
 	fi
 
-	cat << EOF > $tfile
+	cat << EOF > "${tfile}"
 USE mysql;
 FLUSH PRIVILEGES ;
-GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+GRANT ALL ON *.* TO 'root'@'%' identified by "${MARIADB_ROOT_PASSWORD}" WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by "${MARIADB_ROOT_PASSWORD}" WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD("${MARIADB_ROOT_PASSWORD}") ;
+SET PASSWORD FOR 'root'@'%'=PASSWORD("${MARIADB_ROOT_PASSWORD}") ;
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES ;
+
 EOF
 
-	if [ "$MYSQL_DATABASE" != "" ]; then
-	  echo "[i] Creating database: $MYSQL_DATABASE"
-		if [ "$MYSQL_CHARSET" != "" ] && [ "$MYSQL_COLLATION" != "" ]; then
-			echo "[i] with character set [$MYSQL_CHARSET] and collation [$MYSQL_COLLATION]"
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET $MYSQL_CHARSET COLLATE $MYSQL_COLLATION;" >> $tfile
+	if [ "${MARIADB_DATABASE}" != "" ]; then
+	  echo "[i] Creating database: ${MARIADB_DATABASE}"
+		if [ "${MARIADB_CHARSET}" != "" ] && [ "${MARIADB_COLLATION}" != "" ]; then
+			echo "[i] with character set [${MARIADB_CHARSET}] and collation [${MARIADB_COLLATION}]"
+			echo "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\` CHARACTER SET ${MARIADB_CHARSET} COLLATE ${MARIADB_COLLATION};" >> "${tfile}"
 		else
 			echo "[i] with character set: 'utf8' and collation: 'utf8_general_ci'"
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+			echo "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> "${tfile}"
 		fi
 
-	 if [ "$MYSQL_USER" != "" ]; then
-		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-	    fi
+	  if [ "${MARIADB_USER}" != "" ]; then
+		  echo "[i] Creating user: ${MARIADB_USER} with password ${MARIADB_PASSWORD}"
+		  echo "GRANT ALL ON \`${MARIADB_DATABASE}\`.* to '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" >> "${tfile}"
+    fi
 	fi
 
-	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
-	rm -f $tfile
+  for f in /docker-entrypoint-initdb.d/*; do
+  		case "${f}" in
+  			*.sql)
+  			  echo "$0: running ${f}";
+  			  cat "${f}" >> "${tfile}"
+  			  echo
+  			  ;;
 
-	for f in /docker-entrypoint-initdb.d/*; do
-		case "$f" in
-			*.sql)    echo "$0: running $f"; /usr/bin/mysqld --user=root -p'$MYSQL_PASSWORD' --verbose=0 --skip-name-resolve --skip-networking=0 < "$f"; echo ;;
-			*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | /usr/bin/mysqld --user=root -p'$MYSQL_PASSWORD' --verbose=0 --skip-name-resolve --skip-networking=0 < "$f"; echo ;;
-			*)        echo "$0: ignoring or entrypoint initdb empty $f" ;;
-		esac
-		echo
-	done
+  			*.sql.gz)
+  			  echo "$0: running ${f}";
+  			  gunzip -c "${f}" >> "${tfile}"
+  			  echo
+  			  ;;
+
+  			*)
+  			  echo "$0: ignoring or entrypoint initdb empty $f";
+  			  echo
+  			  ;;
+  		esac
+  		echo
+  	done
+
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "${tfile}"
+	rm -f "${tfile}"
 
 	echo
 	echo 'MySQL init process done. Ready for start up.'
 	echo
 
-	echo "exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0" "$@"
+	echo "exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0" "${@}"
 fi
 
 # execute any pre-exec scripts
 for i in /usr/local/bin/pre-exec.d/*sh
 do
 	if [ -e "${i}" ]; then
-		echo "[i] pre-exec.d - processing $i"
-		. ${i}
+		echo "[i] pre-exec.d - processing ${i}"
+		# shellcheck disable=SC1090
+		. "${i}"
 	fi
 done
 
-exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 "${@}"
