2634012599ba71fda6f5aaa13a08bdea6ea6067e
Dimitar Nikov <dn@20i.com>
2024-07-21 18:19:53 +0100

    Update base image to 3.20.1

--- alpine/base/build.sh
+++ alpine/base/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-docker build --no-cache -t arruor/alpine:latest -t arruor/alpine:3.19 -t arruor/alpine:3.19.0 .
+docker build --no-cache -t arruor/alpine:latest -t arruor/alpine:3.20 -t arruor/alpine:3.20.1 .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
 fi
 
 docker push arruor/alpine:latest
-docker push arruor/alpine:3.19
-docker push arruor/alpine:3.19.0
+docker push arruor/alpine:3.20
+docker push arruor/alpine:3.20.1
