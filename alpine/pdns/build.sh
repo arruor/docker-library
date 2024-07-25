230c4751e170db681b5e6f981a513ec9af65bef8
Dimitar Nikov <dn@20i.com>
2024-07-25 18:44:37 +0100

    Rebuild PowerDNS image on version 4.9.1

--- alpine/pdns/build.sh
+++ alpine/pdns/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-docker build --no-cache -t arruor/pdns:latest -t arruor/pdns:4.8 -t arruor/pdns:4.8.3 .
+docker build --no-cache -t arruor/pdns:latest -t arruor/pdns:4.9 -t arruor/pdns:4.9.1 .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
 fi
 
 docker push arruor/pdns:latest
-docker push arruor/pdns:4.8
-docker push arruor/pdns:4.8.3
+docker push arruor/pdns:4.9
+docker push arruor/pdns:4.9.1
