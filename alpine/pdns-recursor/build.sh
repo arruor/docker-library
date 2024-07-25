118564d70bcd6cca4281042f7735bc95aaf861c2
Dimitar Nikov <dn@20i.com>
2024-07-25 18:45:40 +0100

    Rebuild PowerDNS Recursor image on version 5.0.5

--- alpine/pdns-recursor/build.sh
+++ alpine/pdns-recursor/build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-docker build --no-cache -t arruor/pdns-recursor:latest -t arruor/pdns-recursor:4.9 -t arruor/pdns-recursor:4.9.2 .
+docker build --no-cache -t arruor/pdns-recursor:latest -t arruor/pdns-recursor:5.0 -t arruor/pdns-recursor:5.0.5 .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
 fi
 
 docker push arruor/pdns-recursor:latest
-docker push arruor/pdns-recursor:4.9
-docker push arruor/pdns-recursor:4.9.2
+docker push arruor/pdns-recursor:5.0.5
+docker push arruor/pdns-recursor:5.0
+docker push arruor/pdns-recursor:5
