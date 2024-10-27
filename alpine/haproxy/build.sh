b542d4d531673f0383a83ebcd0a0d88e9dbfe54c
Dimitar Nikov <dn@20i.com>
2024-10-27 10:28:11 +0000

    Update HAProxy to 3.0.5

--- alpine/haproxy/build.sh
+++ alpine/haproxy/build.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+TAG_SHORT=3.0
+TAG_LONG=3.0.5
+
 set -euo pipefail
 
-docker build --no-cache -t arruor/haproxy:latest -t arruor/haproxy:2.8 -t arruor/haproxy:2.8.5 .
+docker build --no-cache -t arruor/haproxy:latest -t arruor/haproxy:${TAG_SHORT} -t arruor/haproxy:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
 fi
 
-docker push arruor/haproxy:2.8.5
-docker push arruor/haproxy:2.8
+docker push arruor/haproxy:${TAG_LONG}
+docker push arruor/haproxy:${TAG_SHORT}
 docker push arruor/haproxy:latest
