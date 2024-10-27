0185f9b87c8a11f4500c5df22efbc2f8f36eb38a
Dimitar Nikov <dn@20i.com>
2024-10-27 10:08:43 +0000

    Update Alpine base image to 3.20.3

--- alpine/base/build.sh
+++ alpine/base/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-docker build --no-cache -t arruor/alpine:latest -t arruor/alpine:3.20 -t arruor/alpine:3.20.1 .
+TAG_SHORT=3.20
+TAG_LONG=3.20.3
+docker build --no-cache -t arruor/alpine:latest -t arruor/alpine:${TAG_SHORT} -t arruor/alpine:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
 fi
 
 docker push arruor/alpine:latest
-docker push arruor/alpine:3.20
-docker push arruor/alpine:3.20.1
+docker push arruor/alpine:${TAG_SHORT}
+docker push arruor/alpine:${TAG_LONG}
