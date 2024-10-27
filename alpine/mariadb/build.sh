80ef20c1d542decbbe45365ac7a284c937d71bb1
Dimitar Nikov <dn@20i.com>
2024-10-27 10:56:20 +0000

    Rebuild MariaDB Image on Alpine Linux 3.20.3

--- alpine/mariadb/build.sh
+++ alpine/mariadb/build.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-docker build --no-cache -t arruor/mariadb:latest -t arruor/mariadb:10.11 -t arruor/mariadb:10.11.8 .
+
+TAG_SHORT=10.11
+TAG_LONG=10.11.8
+
+set -euo pipefail
+
+docker build --no-cache -t arruor/mariadb:latest -t arruor/mariadb:${TAG_SHORT} -t arruor/mariadb:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
 fi
 
 docker push arruor/mariadb:latest
-docker push arruor/mariadb:10.11
-docker push arruor/mariadb:10.11.8
+docker push arruor/mariadb:${TAG_SHORT}
+docker push arruor/mariadb:${TAG_LONG}
