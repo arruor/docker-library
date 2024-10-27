9fcac31e5552abf0ad3855f1a2ddb40a44834328
Dimitar Nikov <dn@20i.com>
2024-10-27 12:40:55 +0000

    Update php-cli image to 8.3.13

--- alpine/php/cli/build.sh
+++ alpine/php/cli/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+TAG_SHORT=8.3
+TAG_LONG=8.3.13
+
+set -euo pipefail
+
 docker build --no-cache -t arruor/php-cli:latest \
-  -t arruor/php-cli:8.2.12 \
-  -t arruor/php-cli:8.2 \
+  -t arruor/php-cli:${TAG_LONG} \
+  -t arruor/php-cli:${TAG_SHORT} \
   -t arruor/php-cli:8 \
   .
 
@@ -11,6 +16,6 @@ if [ ${?} -ne 0 ]; then
 fi
 
 docker push arruor/php-cli:latest
-docker push arruor/php-cli:8.2.12
-docker push arruor/php-cli:8.2
+docker push arruor/php-cli:${TAG_LONG}
+docker push arruor/php-cli:${TAG_SHORT}
 docker push arruor/php-cli:8
