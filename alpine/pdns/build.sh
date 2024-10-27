74c47f1664de28e0949f1b3a428d6df29c0cfe98
Dimitar Nikov <dn@20i.com>
2024-10-27 11:10:34 +0000

    Update PowerDNS to 4.9.2

--- alpine/pdns/build.sh
+++ alpine/pdns/build.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-docker build --no-cache -t arruor/pdns:latest -t arruor/pdns:4.9 -t arruor/pdns:4.9.1 .
+
+TAG_SHORT=4.9
+TAG_LONG=4.9.2
+
+set -euo pipefail
+
+docker build --no-cache -t arruor/pdns:latest -t arruor/pdns:${TAG_SHORT} -t arruor/pdns:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
 fi
 
 docker push arruor/pdns:latest
-docker push arruor/pdns:4.9
-docker push arruor/pdns:4.9.1
+docker push arruor/pdns:${TAG_SHORT}
+docker push arruor/pdns:${TAG_LONG}
