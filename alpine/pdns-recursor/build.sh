d976193ba443ab203170c8cd076f452f5430570c
Dimitar Nikov <dn@20i.com>
2025-01-02 11:13:32 +0000

    Update pdns-recursor to version 5.1.3

--- alpine/pdns-recursor/build.sh
+++ alpine/pdns-recursor/build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-TAG_SHORT=5.0
-TAG_LONG=5.09
+TAG_SHORT=5.1
+TAG_LONG=5.1.3
+
+BUILD_DATE=$(date "+%Y-%m-%d")
+VCS_REF=$(git lo -1|awk '{print $2}')
+VER="3.21.0"
 
 set -euo pipefail
 
-docker build --no-cache -t arruor/pdns-recursor:latest -t arruor/pdns-recursor:${TAG_SHORT} -t arruor/pdns-recursor:${TAG_LONG} .
+docker build --no-cache \
+ --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} --build-arg VER=${VER} \
+ -t arruor/pdns-recursor:latest -t arruor/pdns-recursor:${TAG_SHORT} -t arruor/pdns-recursor:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
