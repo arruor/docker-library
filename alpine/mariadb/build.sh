78cf3f3a8af8712b17378fcf6f412c73744b8f55
Dimitar Nikov <dn@20i.com>
2025-01-02 10:45:39 +0000

    Update MariaDB to version 11.4.4

--- alpine/mariadb/build.sh
+++ alpine/mariadb/build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-TAG_SHORT=10.11
-TAG_LONG=10.11.8
+TAG_SHORT=11.4
+TAG_LONG=11.4.4
+BUILD_DATE=$(date "+%Y-%m-%d")
+VCS_REF=$(git lo -1|awk '{print $2}')
+VER="3.21.0"
 
 set -euo pipefail
 
-docker build --no-cache -t arruor/mariadb:latest -t arruor/mariadb:${TAG_SHORT} -t arruor/mariadb:${TAG_LONG} .
+docker build --no-cache \
+--build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} --build-arg VER=${VER} \
+ -t arruor/mariadb:latest -t arruor/mariadb:${TAG_SHORT} -t arruor/mariadb:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
