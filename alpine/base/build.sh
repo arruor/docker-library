8e379ce1509e08e1394b857acd94634923355f55
Dimitar Nikov <dn@20i.com>
2025-01-02 10:32:25 +0000

    Fix base image build script

--- alpine/base/build.sh
+++ alpine/base/build.sh
@@ -4,9 +4,9 @@ TAG_LONG=3.21.0
 BUILD_DATE=$(date "+%Y-%m-%d")
 VCS_REF=$(git lo -1|awk '{print $2}')
 
-docker build --no-cache --platform=linux/amd64,linux/arm64 \
+docker build --no-cache \
   --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} \
-  arruor/alpine:latest -t arruor/alpine:${TAG_SHORT} -t arruor/alpine:${TAG_LONG} .
+  -t arruor/alpine:latest -t arruor/alpine:${TAG_SHORT} -t arruor/alpine:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
