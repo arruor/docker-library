adac4348715d5664fa7911f6403de3e2815c0ad6
Dimitar Nikov <dn@20i.com>
2025-01-02 11:08:39 +0000

    Upgrade base image to 3.21.0 for pdns image

--- alpine/pdns/build.sh
+++ alpine/pdns/build.sh
@@ -2,10 +2,15 @@
 
 TAG_SHORT=4.9
 TAG_LONG=4.9.2
+BUILD_DATE=$(date "+%Y-%m-%d")
+VCS_REF=$(git lo -1|awk '{print $2}')
+VER="3.21.0"
 
 set -euo pipefail
 
-docker build --no-cache -t arruor/pdns:latest -t arruor/pdns:${TAG_SHORT} -t arruor/pdns:${TAG_LONG} .
+docker build --no-cache \
+ --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} --build-arg VER=${VER} \
+ -t arruor/pdns:latest -t arruor/pdns:${TAG_SHORT} -t arruor/pdns:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
