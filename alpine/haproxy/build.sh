36cd999eb22798a230f7aeaf5e2191853f14d6b6
Dimitar Nikov <dn@20i.com>
2025-01-03 09:53:42 +0000

    Add logging to HAProxy image

--- alpine/haproxy/build.sh
+++ alpine/haproxy/build.sh
@@ -4,11 +4,12 @@ TAG_SHORT=3.1
 TAG_LONG=3.1.1
 BUILD_DATE=$(date "+%Y-%m-%d")
 VCS_REF=$(git lo -1|awk '{print $2}')
+VER="3.21.0"
 
 set -euo pipefail
 
 docker build --no-cache \
- --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} \
+ --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} --build-arg VER=${VER} \
  -t arruor/haproxy:latest -t arruor/haproxy:${TAG_SHORT} -t arruor/haproxy:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
--- alpine/haproxy/docker-entrypoint.sh
+++ alpine/haproxy/docker-entrypoint.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
 set -euo pipefail
+set -o errexit
+set -o nounset
 
 # first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- haproxy "$@"
-fi
+##if [ "${1#-}" != "$1" ]; then
+##	set -- haproxy "$@"
+##fi
 
-if [ "$1" = 'haproxy' ]; then
-	shift # "haproxy"
+##if [ "$1" = 'haproxy' ]; then
+##	shift # "haproxy"
 	# if the user wants "haproxy", let's add a couple useful flags
 	#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
 	#   -db -- disables background mode
-	set -- haproxy -W -db "$@"
-fi
+##	set -- haproxy -W -db "$@"
+##fi
 
-exec "$@"
+##exec "$@"
+
+readonly RSYSLOG_PID="/var/run/rsyslogd.pid"
+
+main() {
+  start_rsyslogd
+  start_lb "$@"
+}
+
+start_rsyslogd() {
+  rm -f $RSYSLOG_PID
+  rsyslogd
+}
+
+start_lb() {
+  exec haproxy -W -db "$@"
+}
+
+main "$@"
