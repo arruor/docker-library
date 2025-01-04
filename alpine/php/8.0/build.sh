05a867c8569cfde098970a81d7b5add309bf09e9
Dimitar Nikov <dn@20i.com>
2025-01-04 09:49:51 +0000

    Add PHP 8.0 image

---
+++ alpine/php/8.0/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+TAG_SHORT=8.0
+TAG_LONG=8.0.30
+
+BUILD_DATE=$(date "+%Y-%m-%d")
+VCS_REF=$(git lo -1|awk '{print $2}')
+VER="3.21.0"
+
+set -euo pipefail
+
+docker build --no-cache \
+  --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} --build-arg VER=${VER} \
+  -t arruor/php-8.0:latest \
+  -t arruor/php-8.0:${TAG_LONG} \
+  -t arruor/php-8.0:${TAG_SHORT} \
+  .
+
+if [ ${?} -ne 0 ]; then
+  exit ${?}
+fi
+
+docker push arruor/php-8.0:latest
+docker push arruor/php-8.0:${TAG_LONG}
+docker push arruor/php-8.0:${TAG_SHORT}
