ee3cb8e64085169dbd8b7574fdf58ed19654d6fc
Dimitar Nikov <dn@20i.com>
2025-07-31 12:22:13 +0100

    Add MariaDB 11.8 repo to base image

--- almalinux/base/build.sh
+++ almalinux/base/build.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 TAG_SHORT=9
-TAG_LONG="9.5"
-VER="9.5.202503"
-TAG_BASE="arruor/almalinux"
+TAG_LONG="9.6"
+VER="1.0.1"
+TAG_BASE="hub.lhr.stackcp.net/20i/almalinux"
 BUILD_DATE=$(date "+%Y-%m-%d")
 VCS_REF=$(git lo -1|awk '{print $2}')
 
@@ -14,6 +14,10 @@ docker build --no-cache \
   --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} \
   --build-arg VER=${VER} \
   -t ${TAG_BASE}:latest -t ${TAG_BASE}:${TAG_SHORT} -t ${TAG_BASE}:${TAG_LONG} .
+
+if [ ${?} -ne 0 ]; then
+  exit ${?}
+fi
 
 docker push ${TAG_BASE}:latest
 docker push ${TAG_BASE}:${TAG_SHORT}
