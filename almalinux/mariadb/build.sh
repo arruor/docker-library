b5bd1eefa4a3bd68f0541aa73c08f366da255b85
Dimitar Nikov <dn@20i.com>
2025-07-31 16:02:49 +0100

    Fix meta data for Alma Linux MariaDB Image

--- almalinux/mariadb/build.sh
+++ almalinux/mariadb/build.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-TAG_SHORT=11.8
-TAG_LONG=11.8.2
-VER="11.8.2"
-TAG_BASE="hub.lhr.stackcp.net/20i/almalinux"
+set -euo pipefail
+
+TAG_SHORT=11
+TAG_LONG="11.8"
+VERSION="11.8.2"
+TAG_BASE="hub.lhr.stackcp.net/20i/mariadb"
 BUILD_DATE=$(date "+%Y-%m-%d")
 VCS_REF=$(git lo -1|awk '{print $2}')
 
-set -euo pipefail
-
 docker build --no-cache \
   --platform linux/amd64,linux/arm64 \
-  --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} \
-  --build-arg VER=${VER} \
-  -t ${TAG_BASE}:latest -t ${TAG_BASE}:${TAG_SHORT} -t ${TAG_BASE}:${TAG_LONG} .
+  --build-arg BUILD_DATE=${BUILD_DATE} \
+  --build-arg VCS_REF=${VCS_REF} \
+  --build-arg VERSION=${VERSION} \
+  -t ${TAG_BASE}:latest \
+  -t ${TAG_BASE}:${TAG_SHORT} \
+  -t ${TAG_BASE}:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
