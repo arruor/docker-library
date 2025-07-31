8cfaf10787053b37330028d211da46830d5df58f
Dimitar Nikov <dn@20i.com>
2025-07-31 17:12:55 +0100

    Migrate build to docker buildx

--- almalinux/base/build.sh
+++ almalinux/base/build.sh
@@ -3,19 +3,18 @@
 set -euo pipefail
 
 VERSION="1.0.1"
-TAG_BASE="hub.lhr.stackcp.net/20i/alma"
+IMAGE_NAME="hub.lhr.stackcp.net/20i/alma"
 BUILD_DATE=$(date "+%Y-%m-%d")
-VCS_REF=$(git lo -1|awk '{print $2}')
+VCS_REF=$(git rev-parse --short HEAD)
+VCS_URL="https://github.com/arruor/docker-library/almalinux/base"
 
-docker build --no-cache \
+docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  --build-arg BUILD_DATE=${BUILD_DATE} \
-  --build-arg VCS_REF=${VCS_REF} \
-  --build-arg VERSION=${VERSION} \
-  -t ${TAG_BASE}:latest .
-
-if [ ${?} -ne 0 ]; then
-  exit ${?}
-fi
-
-docker push ${TAG_BASE}:latest
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg VCS_REF="${VCS_REF}" \
+  --build-arg VERSION="${VERSION}" \
+  --build-arg VCS_URL="${VCS_URL}" \
+  --push \
+  -t "${IMAGE_NAME}:${VERSION}" \
+  -t "${IMAGE_NAME}:latest" \
+  -f Dockerfile .
