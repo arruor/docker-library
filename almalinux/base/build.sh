2b1c04ef49d2fdda8343f4de2f094f23a9887d22
Dimitar Nikov <dn@20i.com>
2025-07-31 15:57:48 +0100

    Fix meta data for Alma Linux Base Image

--- almalinux/base/build.sh
+++ almalinux/base/build.sh
@@ -4,16 +4,19 @@ set -euo pipefail
 
 TAG_SHORT=9
 TAG_LONG="9.6"
-VER="1.0.1"
-TAG_BASE="hub.lhr.stackcp.net/20i/almalinux"
+VERSION="1.0.1"
+TAG_BASE="hub.lhr.stackcp.net/20i/alma"
 BUILD_DATE=$(date "+%Y-%m-%d")
 VCS_REF=$(git lo -1|awk '{print $2}')
 
 docker build --no-cache \
   --platform linux/amd64,linux/arm64 \
-  --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} \
-  --build-arg VER=${VER} \
-  -t ${TAG_BASE}:latest -t ${TAG_BASE}:${TAG_SHORT} -t ${TAG_BASE}:${TAG_LONG} .
+  --build-arg BUILD_DATE=${BUILD_DATE} \
+  --build-arg VCS_REF=${VCS_REF} \
+  --build-arg VERSION=${VERSION} \
+  -t ${TAG_BASE}:latest \
+  -t ${TAG_BASE}:${TAG_SHORT} \
+  -t ${TAG_BASE}:${TAG_LONG} .
 
 if [ ${?} -ne 0 ]; then
   exit ${?}
