b01b0c6f4952d13ce920b10e86e5b27ffa38929d
Dimitar Nikov <dn@20i.com>
2025-08-01 16:29:10 +0100

    Do better cleanup on PowerDNS image

--- almalinux/pdns/build.sh
+++ almalinux/pdns/build.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env bash
 
-TAG_SHORT=4
-TAG_LONG=4.8
-BUILD_DATE=$(date "+%Y-%m-%d")
-TAG_BASE="hub.lhr.stackcp.net/20i/pdns"
-VCS_REF=$(git lo -1|awk '{print $2}')
-VERSION="4.8.4"
-
 set -euo pipefail
 
-docker build --no-cache \
+VERSION="4.8.4"
+IMAGE_NAME="hub.lhr.stackcp.net/20i/pdns"
+BUILD_DATE=$(date "+%Y-%m-%d")
+VCS_REF=$(git rev-parse --short HEAD)
+VCS_URL="https://github.com/arruor/docker-library/almalinux/pdns"
+
+docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  --build-arg BUILD_DATE=${BUILD_DATE} \
-  --build-arg VCS_REF=${VCS_REF} \
-  --build-arg VERSION=${VERSION} \
-  -t ${TAG_BASE}:latest \
-  -t ${TAG_BASE}:${TAG_SHORT} \
-  -t ${TAG_BASE}:${TAG_LONG} .
-
-if [ ${?} -ne 0 ]; then
-  exit ${?}
-fi
-
-docker push ${TAG_BASE}:latest
-docker push ${TAG_BASE}:${TAG_SHORT}
-docker push ${TAG_BASE}:${TAG_LONG}
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg VCS_REF="${VCS_REF}" \
+  --build-arg VERSION="${VERSION}" \
+  --build-arg VCS_URL="${VCS_URL}" \
+  --push \
+  -t "${IMAGE_NAME}:${VERSION}" \
+  -t "${IMAGE_NAME}:latest" \
+  -f Dockerfile .
