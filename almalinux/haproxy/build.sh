60b68398dc22ce0bc4db9842349b19d02fb58701
Dimitar Nikov <dn@20i.com>
2025-08-01 15:12:56 +0100

    Improve build performance for HAProxy AL9 image

--- almalinux/haproxy/build.sh
+++ almalinux/haproxy/build.sh
@@ -2,26 +2,19 @@
 
 set -euo pipefail
 
-TAG_SHORT=3
-TAG_LONG="3.2"
 VERSION="3.2.3"
 TAG_BASE="hub.lhr.stackcp.net/20i/haproxy"
 BUILD_DATE=$(date "+%Y-%m-%d")
-VCS_REF=$(git lo -1|awk '{print $2}')
+VCS_REF=$(git rev-parse --short HEAD)
+VCS_URL="https://github.com/arruor/docker-library/almalinux/haproxy"
 
-docker build --no-cache \
+docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  --build-arg BUILD_DATE=${BUILD_DATE} \
-  --build-arg VCS_REF=${VCS_REF} \
-  --build-arg VERSION=${VERSION} \
-  -t ${TAG_BASE}:latest \
-  -t ${TAG_BASE}:${TAG_SHORT} \
-  -t ${TAG_BASE}:${TAG_LONG} .
-
-if [ ${?} -ne 0 ]; then
-  exit ${?}
-fi
-
-docker push ${TAG_BASE}:latest
-docker push ${TAG_BASE}:${TAG_SHORT}
-docker push ${TAG_BASE}:${TAG_LONG}
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg VCS_REF="${VCS_REF}" \
+  --build-arg VERSION="${VERSION}" \
+  --build-arg VCS_URL="${VCS_URL}" \
+  --push \
+  -t "${IMAGE_NAME}:${VERSION}" \
+  -t "${IMAGE_NAME}:latest" \
+  -f Dockerfile .
