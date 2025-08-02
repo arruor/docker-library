5ff8f1cae10f33c2d5e67898d4cc4bc0286a543f
Dimitar Nikov <dn@20i.com>
2025-08-02 11:07:26 +0100

    Fix bad check for mysql database in MariaDB image

--- almalinux/mariadb/startup.sh
+++ almalinux/mariadb/startup.sh
@@ -28,22 +28,23 @@ else
 	chown -R mysql:mysql /var/lib/mysql
 
 	mariadb-install-db --user=mysql --ldata=/var/lib/mysql > /dev/null
+fi
 
-	if [ "${MARIADB_ROOT_PASSWORD}" = "" ]; then
-		MARIADB_ROOT_PASSWORD=$(pwgen 16 1)
-		echo "[i] MySQL root Password: ${MARIADB_ROOT_PASSWORD}"
-	fi
+if [ "${MARIADB_ROOT_PASSWORD}" = "" ]; then
+  MARIADB_ROOT_PASSWORD=$(pwgen 16 1)
+  echo "[i] MySQL root Password: ${MARIADB_ROOT_PASSWORD}"
+fi
 
-	MARIADB_DATABASE=${MARIADB_DATABASE:-""}
-	MARIADB_USER=${MARIADB_USER:-""}
-	MARIADB_PASSWORD=${MARIADB_PASSWORD:-""}
+MARIADB_DATABASE=${MARIADB_DATABASE:-""}
+MARIADB_USER=${MARIADB_USER:-""}
+MARIADB_PASSWORD=${MARIADB_PASSWORD:-""}
 
-	tfile=$(mktemp)
-	if [ ! -f "${tfile}" ]; then
-	    return 1
-	fi
+tfile=$(mktemp)
+if [ ! -f "${tfile}" ]; then
+  return 1
+fi
 
-	cat << EOF > "${tfile}"
+cat << EOF > "${tfile}"
 USE mysql;
 FLUSH PRIVILEGES ;
 GRANT ALL ON *.* TO 'root'@'%' identified by "${MARIADB_ROOT_PASSWORD}" WITH GRANT OPTION ;
@@ -55,53 +56,52 @@ FLUSH PRIVILEGES ;
 
 EOF
 
-	if [ "${MARIADB_DATABASE}" != "" ]; then
-	  echo "[i] Creating database: ${MARIADB_DATABASE}"
-		if [ "${MARIADB_CHARSET}" != "" ] && [ "${MARIADB_COLLATION}" != "" ]; then
-			echo "[i] with character set [${MARIADB_CHARSET}] and collation [${MARIADB_COLLATION}]"
-			echo "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\` CHARACTER SET ${MARIADB_CHARSET} COLLATE ${MARIADB_COLLATION};" >> "${tfile}"
-		else
-			echo "[i] with character set: 'utf8' and collation: 'utf8_general_ci'"
-			echo "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> "${tfile}"
-		fi
+if [ "${MARIADB_DATABASE}" != "" ]; then
+  echo "[i] Creating database: ${MARIADB_DATABASE}"
+  if [ "${MARIADB_CHARSET}" != "" ] && [ "${MARIADB_COLLATION}" != "" ]; then
+    echo "[i] with character set [${MARIADB_CHARSET}] and collation [${MARIADB_COLLATION}]"
+    echo "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\` CHARACTER SET ${MARIADB_CHARSET} COLLATE ${MARIADB_COLLATION};" >> "${tfile}"
+  else
+    echo "[i] with character set: 'utf8' and collation: 'utf8_general_ci'"
+    echo "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> "${tfile}"
+  fi
 
-	  if [ "${MARIADB_USER}" != "" ]; then
-		  echo "[i] Creating user: ${MARIADB_USER} with password ${MARIADB_PASSWORD}"
-		  echo "GRANT ALL ON \`${MARIADB_DATABASE}\`.* to '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" >> "${tfile}"
-    fi
-	fi
-
-  for f in /docker-entrypoint-initdb.d/*; do
-  		case "${f}" in
-  			*.sql)
-  			  echo "$0: running ${f}";
-  			  cat "${f}" >> "${tfile}"
-  			  echo
-  			  ;;
-
-  			*.sql.gz)
-  			  echo "$0: running ${f}";
-  			  gunzip -c "${f}" >> "${tfile}"
-  			  echo
-  			  ;;
-
-  			*)
-  			  echo "$0: ignoring or entrypoint initdb empty $f";
-  			  echo
-  			  ;;
-  		esac
-  		echo
-  	done
-
-	/usr/sbin/mariadbd --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "${tfile}"
-	rm -f "${tfile}"
-
-	echo
-	echo 'MySQL init process done. Ready for start up.'
-	echo
-
-	echo "exec /usr/sbin/mariadbd --user=mysql --console --skip-name-resolve --skip-networking=0" "${@}"
+  if [ "${MARIADB_USER}" != "" ]; then
+    echo "[i] Creating user: ${MARIADB_USER} with password ${MARIADB_PASSWORD}"
+    echo "GRANT ALL ON \`${MARIADB_DATABASE}\`.* to '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" >> "${tfile}"
+  fi
 fi
+
+for f in /docker-entrypoint-initdb.d/*; do
+  case "${f}" in
+    *.sql)
+      echo "$0: running ${f}";
+      cat "${f}" >> "${tfile}"
+      echo
+      ;;
+
+    *.sql.gz)
+      echo "$0: running ${f}";
+      gunzip -c "${f}" >> "${tfile}"
+      echo
+      ;;
+
+    *)
+      echo "$0: ignoring or entrypoint initdb empty $f";
+      echo
+      ;;
+  esac
+  echo
+done
+
+/usr/sbin/mariadbd --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "${tfile}"
+rm -f "${tfile}"
+
+echo
+echo 'MySQL init process done. Ready for start up.'
+echo
+
+echo "exec /usr/sbin/mariadbd --user=mysql --console --skip-name-resolve --skip-networking=0" "${@}"
 
 # execute any pre-exec scripts
 for i in /usr/local/bin/pre-exec.d/*sh
