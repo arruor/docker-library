fbcaec6151d9510abd0dbaaaef901461445bfa29
Dimitar Nikov <dn@20i.com>
2025-08-03 14:33:06 +0100

    Add PHP 8.3 images on Alma Linux 9

---
+++ almalinux/php/8.3/cli/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VERSION="8.3.24"
+IMAGE_NAME="hub.lhr.stackcp.net/20i/php-8.3-cli"
+BUILD_DATE=$(date "+%Y-%m-%d")
+VCS_REF=$(git rev-parse --short HEAD)
+VCS_URL="https://github.com/arruor/docker-library/almalinux/php-8.3/cli"
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg VCS_REF="${VCS_REF}" \
+  --build-arg VERSION="${VERSION}" \
+  --build-arg VCS_URL="${VCS_URL}" \
+  --push \
+  -t "${IMAGE_NAME}:${VERSION}" \
+  -t "${IMAGE_NAME}:latest" \
+  -f Dockerfile .
