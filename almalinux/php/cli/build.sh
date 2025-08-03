a11ee34b51452ce21acc49248cd50c9baeafa194
Dimitar Nikov <dn@20i.com>
2025-08-03 09:33:36 +0100

    Add PHP images to the mix

---
+++ almalinux/php/cli/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+TAG_SHORT=8.4
+TAG_LONG=8.4.4
+
+BUILD_DATE=$(date "+%Y-%m-%d")
+VCS_REF=$(git lo -1|awk '{print $2}')
+VER="3.21.3"
+
+set -euo pipefail
+
+docker build --no-cache \
+  --platform linux/amd64,linux/arm64 \
+  --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF} --build-arg VER=${VER} \
+  -t arruor/php-cli:latest \
+  -t arruor/php-cli:${TAG_LONG} \
+  -t arruor/php-cli:${TAG_SHORT} \
+  -t arruor/php-cli:8 \
+  .
+
+if [ ${?} -ne 0 ]; then
+  exit ${?}
+fi
+
+docker push arruor/php-cli:latest
+docker push arruor/php-cli:${TAG_LONG}
+docker push arruor/php-cli:${TAG_SHORT}
+docker push arruor/php-cli:8
