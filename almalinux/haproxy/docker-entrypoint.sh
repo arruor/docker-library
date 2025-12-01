1b6201f8fe5645d62957dd7e08daa0d982abda4b
Dimitar Nikov <dn@20i.com>
2025-12-01 17:14:04 +0000

    Add -W to HAProxy startup params

--- almalinux/haproxy/docker-entrypoint.sh
+++ almalinux/haproxy/docker-entrypoint.sh
@@ -6,18 +6,18 @@ rsyslogd
 
 # If no arguments are passed, default to haproxy
 if [ "$#" -eq 0 ]; then
-    exec haproxy -db -f /etc/haproxy/haproxy.cfg
+    exec haproxy -W -db -f /etc/haproxy/haproxy.cfg
 fi
 
 # If first argument starts with '-', assume haproxy args
 if [[ "${1:-}" == -* ]]; then
-    exec haproxy -db "$@"
+    exec haproxy -W -db "$@"
 fi
 
 # If first argument is 'haproxy', run it with useful defaults
 if [[ "${1:-}" == "haproxy" ]]; then
     shift
-    exec haproxy -db "$@"
+    exec haproxy -W -db "$@"
 fi
 
 # Otherwise, run user command
